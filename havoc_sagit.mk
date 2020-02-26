f2be3d3eff176c3748ce018e7f76f64048c5c966
EnderXH <enderhx5@gmail.com>
2020-02-26 16:38:23 +0500

    sagit: havoc bringup

--- havoc_sagit.mk
+++ havoc_sagit.mk
@@ -21,14 +21,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sagit device
 $(call inherit-product, device/xiaomi/sagit/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sagit
+PRODUCT_NAME := havoc_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Official
+export export HAVOC_BUILD_TYPE=Official
+ro.havoc.maintainer=EnderXH
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
